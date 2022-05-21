a22aa8cd029ac67e39e5f557160758b2c20941ea
UdoetukEkemini <udoetukekemini@gmail.com>
2022-05-21 15:02:09 +0100

    python program for sum of two numbers

---
+++ add two numbers together.py
@@ -0,0 +1,14 @@
+#add two numbers togther
+first_number = input("Enter your first number here: ")
+int("first_number")
+print("First number is", first_number)
+
+
+second_number = input("Enter your second number here: ")
+int("second_number")
+print("Second number is", second_number)
+int(second_number)
+
+sum = (first_number + second_number)
+
+print("The sum of both numbers is", sum)
