64c596ab2ed34762a02483c7b8e137b8426feb9b
Ekemini Udoetuk <98382760+Udoetuk@users.noreply.github.com>
2022-05-30 10:13:39 +0100

    Update add two numbers together.py

--- add two numbers together.py
+++ add two numbers together.py
@@ -1,11 +1,9 @@
 #add two numbers togther
-first_number = input("Enter your first number here: ")
-int("first_number")
+first_number = int(input("Enter your first number here: "))
 print("First number is", first_number)
 
 
-second_number = input("Enter your second number here: ")
-int("second_number")
+second_number = int(input("Enter your second number here: "))
 print("Second number is", second_number)
 
 
